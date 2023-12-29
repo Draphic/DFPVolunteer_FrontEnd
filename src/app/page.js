bf7682957914890d4c7ba9ba36b73df50c602d33
Kalih27 <zpqm1065@gmail.com>
2023-12-29 17:24:57 -0500

    Fix tailwind and begin work on email entry -known issue where there is large gap in middle of page

--- src/app/page.js
+++ src/app/page.js
@@ -1,19 +1,89 @@
-"use client"
-import {NextUIProvider} from "@nextui-org/react";
-import { Button } from '@nextui-org/react'
+"use client";
+import { NextUIProvider } from "@nextui-org/react";
+import { Button, Input } from "@nextui-org/react";
+import React, { useState } from "react";
 
+function MinFontCalc({ pixelMin }, { percentChg }) {
+  return "calc(" + { pixelMin } + "px + " + { percentChg } + "vw)";
+}
 
-function FrontPageHeader({title}){
-  return(<h1>{title}</h1>)
+function FrontPageHeader({ title }) {
+  return <h1 style={{ "font-size": "calc(12px + 1.5vw)" }}>{title}</h1>;
 }
 
 export default function Home() {
+  const [email, setEmail] = useState("");
+  //const out = getStreamSomehow();
+  //const err = getStreamSomehow();
+  //const myConsole = new console.Console(out)
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.error("email: %s", email);
+    // const result = await signIn('credentials', {
+    //   redirect: false,
+    //   email,
+    //   password,
+    // })
+    // if (!result.error) {
+    //   router.push('/')
+    // }
+  };
+
+  const validateEmail = (value) =>
+    value.match(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+.[A-Z]{2,4}$/i);
+
+  const isEmailInvalid = React.useMemo(() => {
+    if (email === "") return false;
+
+    return validateEmail(email) ? false : true;
+  }, [email]);
+
   return (
     <NextUIProvider>
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-    <FrontPageHeader title="Dublin Food Pantry Volunteer Sign-In"/>
-    <Button>CLICK ME!!!!</Button>
-    </main>
+      <div className="flex min-h-screen flex-col items-center justify-between">
+        <div
+          style={{ "margin-top": "5px + 1vh", "margin-bottom": "5px + 1vh" }}
+        >
+          <FrontPageHeader title="Dublin Food Pantry Volunteer Sign-In" />
+        </div>
+
+        <div style={{ "min-height": "200px" }}>
+          <form onSubmit={handleSubmit}>
+            <Input
+              isRequired
+              isClearable
+              type="email"
+              value={email}
+              onValueChange={setEmail}
+              onClear={() => console.log("email cleared")}
+              placeholder="Enter your email"
+              label="Email"
+              labelPlacement="outside-left"
+              color="default"
+              size="lg"
+              radius="sm"
+              isInvalid={isEmailInvalid}
+              errorMessage={isEmailInvalid && "Please enter a valid email"}
+              classNames={{
+                label: ["text-black/90", "text-4xl"],
+              }}
+            />
+            <div align="center" style={{ "margin-top": "5px" }}>
+              <Button
+                type="submit"
+                style={{
+                  "margin-bottom": "5px + 1vh",
+                  font: "Arial",
+                  "font-size": "24px",
+                }}
+              >
+                Sign in
+              </Button>
+            </div>
+          </form>
+        </div>
+      </div>
     </NextUIProvider>
-  )
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,3 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-  },
-  plugins: [],
-}
-
 // tailwind.config.js
 const {nextui} = require("@nextui-org/react");
 
@@ -25,6 +6,7 @@ module.exports = {
   content: [
     // ...
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+    "./src/app/*.{js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {},
